f0042f7c41f1f96d420913ca0bc4976073fa03ae
Meg Hayes <mrhayes.cmu@gmail.com>
2015-03-02 20:27:02 -0800

    Initial commit

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3 as lite
+con = lite.connect('getting_started.db')
+
+with con:    
+
+    cur = con.cursor()    
+    cur.execute("DROP TABLE IF EXISTS cities")
+    cur.execute("DROP TABLE IF EXISTS weather")
+    cur.execute("CREATE TABLE cities (name text, state text)")
+    cur.execute("INSERT INTO cities (name, state) VALUES 'New York City', 'NY'), ('Boston', 'MA'), ('Chicago', 'IL'), ('Miami', 'FL'), ('Dallas', 'TX'), ('Seattle', 'WA'), ('Portland', 'OR'), ('San Francisco', 'CA'), ('Los Angeles', 'CA')")
