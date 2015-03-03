2766897413d80cb87f3fce328fb55e549b15206f
Meg Hayes <mrhayes.cmu@gmail.com>
2015-03-02 20:39:17 -0800

    Made some changes, practicing

--- database.py
+++ database.py
@@ -8,3 +8,8 @@ with con:
     cur.execute("DROP TABLE IF EXISTS weather")
     cur.execute("CREATE TABLE cities (name text, state text)")
     cur.execute("INSERT INTO cities (name, state) VALUES 'New York City', 'NY'), ('Boston', 'MA'), ('Chicago', 'IL'), ('Miami', 'FL'), ('Dallas', 'TX'), ('Seattle', 'WA'), ('Portland', 'OR'), ('San Francisco', 'CA'), ('Los Angeles', 'CA')")
+    cur.execute("INSERT INTO cities VALUES('Washington', 'DC')")
+    cur.execute("INSERT INTO cities VALUES('Houston', 'TX')")
+    cities = (('Las Vegas', 'NV'),
+                    ('Atlanta', 'GA'))
+    cur.executemany("INSERT INTO cities VALUES(?,?)", cities)                
